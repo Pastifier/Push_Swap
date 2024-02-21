5549f2351ccd4af3abd2fcfe48b6145cfff4c62b
Emran <therealemran.design@gmail.com>
2024-02-21 18:19:08 +0400

    Checks for Valgrind errors in general now.

--- check_script.bash
+++ check_script.bash
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 echo  "Push_Swap:"
-valgrind -s --leak-check=full --show-mismatched-frees=yes ./push_swap $@;
+if valgrind -s --leak-check=full --show-mismatched-frees=yes ./push_swap "$@" 2>&1 | grep -E "ERROR SUMMARY: 0 errors|definitely lost: 0 bytes"; then
+	echo "Valgrind check: OK"
+else
+	echo "Valgrind KO"
+fi
 
-echo -n "Checker: ";
+echo -ne "Checker: ";
 ./push_swap $@ | ./checker_linux $@;
-echo -n "Instructions: "
+echo -ne "Instructions: "
 ./push_swap $@ | wc -l;
