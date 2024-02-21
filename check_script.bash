eb5e525a25fb90df7037c875ae0daf87cee2ef27
Emran <therealemran.design@gmail.com>
2024-02-22 02:50:21 +0400

    Made it display Push_swap's instructions as well

--- check_script.bash
+++ check_script.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo  "Push_Swap:"
+./push_swap $@;
 if valgrind -s --leak-check=full --show-mismatched-frees=yes ./push_swap "$@" 2>&1 | grep -E "ERROR SUMMARY: 0 errors|definitely lost: 0 bytes"; then
 	echo "Valgrind check: OK"
 else
