11c957d6caa1a8c7d6050b061d56d4c81cb21828
Emran <therealemran.design@gmail.com>
2024-02-22 16:57:21 +0400

    Fixed regex on grep

--- check_script.bash
+++ check_script.bash
@@ -2,7 +2,7 @@
 
 echo  "Push_Swap:"
 ./push_swap $@;
-if valgrind -s --leak-check=full --show-mismatched-frees=yes ./push_swap "$@" 2>&1 | grep -E "ERROR SUMMARY: 0 errors|definitely lost: 0 bytes"; then
+if valgrind -s --leak-check=full --show-mismatched-frees=yes ./push_swap "$@" 2>&1 | grep -E "All heap blocks were freed -- no leaks are possible" && ! grep -E "ERROR SUMMARY: [1-9]" <<< "$(valgrind -s --leak-check=full --show-mismatched-frees=yes ./push_swap "$@" 2>&1)"; then
 	echo "Valgrind check: OK"
 else
 	echo "Valgrind KO"
